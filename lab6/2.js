5c8db249d9b90d6397384677b4977c9371278709
Bibek Thokar <bibekthokar1@gmail.com>
2022-06-28 14:06:52 -0500

    lab 6 interval added

--- lab6/2.js
+++ lab6/2.js
@@ -1,17 +1,31 @@
-
-
-const printNumbers = (from, to) => {
-    if (from > to) {
-        return false;
-    }
-    setTimeout(() => {
-        console.log(from);
-        from++;
-        if (from <= to) {
-            printNumbers(from, to);
+// Interval Way
+function printNumbers(from, to) {
+    let current = from;
+    let timerId = setInterval(() => {
+        console.log(current);
+        if (current === to) {
+            clearInterval(timerId);
         }
+        current++;
     }, 1000);
 }
 
+printNumbers(10, 20)
 
-printNumbers(10, 20);
+
+// Timeout way
+// const printNumbers = (from, to) => {
+//     if (from > to) {
+//         return false;
+//     }
+//     setTimeout(() => {
+//         console.log(from);
+//         from++;
+//         if (from <= to) {
+//             printNumbers(from, to);
+//         }
+//     }, 1000);
+// }
+
+
+// printNumbers(10, 20);
